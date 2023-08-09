efdb60e6a4d6b674f4ebcb7b9444e083c7b8aebd
Andrea Poltronieri <andrea.poltronieri21@gmail.com>
2023-08-09 17:16:43 +0100

    add ontology converter utility

--- neon-gpt/utils.py
+++ neon-gpt/utils.py
@@ -3,8 +3,9 @@ Utilities for the neon-gpt package.
 """
 
 import json
-
 from pathlib import Path
+
+from rdflib import Graph
 
 
 def load_prompts(prompts_file: str | Path) -> list:
@@ -20,3 +21,35 @@ def load_prompts(prompts_file: str | Path) -> list:
         prompts = json.load(f)
 
     return prompts
+
+
+def convert_ontology(ontology_file: str | Path,
+                     output_serialisation: str) -> Graph:
+    """
+    Load the ontology from the ontology file.
+    :param ontology_file: The path to the ontology file.
+    :type ontology_file: str | Path
+    :param output_serialisation: The output serialisation format.
+    :type output_serialisation: str
+    :return: The ontology as a string.
+    :rtype: Graph
+    """
+    if isinstance(ontology_file, str):
+        ontology_file = Path(ontology_file)
+
+    with ontology_file.open("r") as f:
+        ontology = f.read()
+
+    # parse the ontology and return the graph
+    try:
+        ontology = Graph().parse(data=ontology, format="ttl")
+    except Exception as e:
+        raise Exception(f"Error parsing the ontology: {e}") from e
+
+    # get the output path, which is the input path with the new extension
+    output_path = ontology_file.with_suffix(f".{output_serialisation.lower()}")
+    # convert the ontology to the desired serialisation
+    ontology = ontology.serialize(format=output_serialisation,
+                                  destination=output_path)
+
+    return ontology
